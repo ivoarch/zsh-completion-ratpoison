b88da9fb85ec56311dac29c429804bc4320b387c
Ivaylo Kuzev <ivkuzev@gmail.com>
2012-10-27 13:24:31 +0200

    add more completion of rpcommands

--- _ratpoison.zsh
+++ _ratpoison.zsh
@@ -1,5 +1,11 @@
 #compdef ratpoison
 #
+# Description:
+# Zsh completion script for the Ratpoison Window Manager.
+#
+# TODO:
+# o add more completion of rpcommands
+#
 # [2005-04-05, 20.49-21.17] by Zrajm C Akfohg (GPL-2 license)
 #
 # TODO:
@@ -22,16 +28,16 @@ typeset -A opt_args
    if [[ $state = cmd ]]; then
 local rpcommands
        rpcommands=(
-	   abort addhook alias bind banish chdir clrunmanaged cnext cprev color
-	   cother curframe definekey dedicate delete delkmap describekey echo
-	   escape exec fdump focus focuslast focusleft focusdown focusright
-	   focusprev focusup fselect gdelete getenv getsel gmerge gmove gnew
-	   gnewbg gnext gprev gravity groups gselect help hsplit inext info iprev
-	   iother kill lastmsg license link listhook meta msgwait newkmap newwm
-	   next nextscreen number only other prev prevscreen prompt putsel quit
-	   ratwarp ratrelwarp ratclick rathold readkey redisplay remhook remove
-	   resize restart rudeness sdump select set setenv sfdump shrink split
-	   source sselect startup_message time title tmpwm unalias unbind
+	   abort addhook alias bind banish banishrel chdir clrunmanaged cnext cprev
+	   colon compat cother curframe definekey def dedicate delete delkmap describekey
+	   echo escape exchangedown exchangeleft exchangeright exchangeup exec execa execf
+	   fdump focus focuslast focusleft focusdown focusright focusprev focusup frestore
+	   fselect gdelete getenv getsel gmerge gmove gnew gnewbg gnext gother gprev gravity grename
+	   groups gselect help hsplit inext info iprev iother kill lastmsg license link listhook
+	   meta msgwait newkmap newwm next nextscreen number only other prev prevscreen prompt
+	   putsel quit ratinfo ratrelinfo ratwarp ratrelwarp ratclick rathold readkey redisplay redo remhook
+	   remove resize restart rudeness sdump select set setenv sfrestore sfdump shrink split
+	   source sselect startup_message swap time title tmpwm unalias unbind
 	   undefinekey undo unmanage unsetenv verbexec version vsplit warp windows
        )
        _values "ratpoison commands" ${(q)rpcommands[@]}
